354460ef1fe69a4e816fa04c10502ccc4051130c
Axellerosh <lubi.axellerosh@gmail.com>
2025-04-06 16:36:56 +0800

    finished layout

--- components/FAQ.jsx
+++ components/FAQ.jsx
@@ -1,13 +1,68 @@
 "use client"
-import React from 'react'
+import React, { useState } from 'react'
 import { Element } from 'react-scroll'
+import CustomTitle from './CustomTitle';
+import { faqs } from '@constants';
+import { FaChevronDown } from '@node_modules/react-icons/fa';
 
 const FAQ = () => {
+   const [activeId, setActiveId] = useState("");
+
+   const handleActiveId = (id) => {
+     if (id === activeId) {
+       setActiveId(null);
+     } else {
+       setActiveId(id);
+     }
+   };
+
   return (
-    <Element name='FAQ'>
-        Faq u
+    <Element name="FAQ">
+      <div className="pt-20 mb-10">
+        <div className="flex-col items-center flex mb-12">
+          <CustomTitle title={"FAQ"} />
+          <h1 className="page_header mb-1">
+            Frequently Asked <span className="text-primary">Questions</span>
+          </h1>
+          <p className="text-xl opacity-65 text-center">
+            Find answers to common questions about our services and process
+          </p>
+        </div>
+
+        <div className="flex justify-center">
+          <div className="grid gap-10 px-10 w-2/3">
+            {faqs.map((faq) => {
+              const active = activeId === faq.id;
+
+              return (
+                <div
+                  key={faq.id}
+                  onClick={() => handleActiveId(faq.id)}
+                  className={`w-full border-b-2 overflow-hidden relative transition-all duration-500 cursor-pointer ${
+                    active ? "h-20 max-sm:h-28" : "h-10"
+                  }`}
+                >
+                  <div className="text-xl font-semibold flex justify-between items-center mb-5">
+                    <p className="text-xl max-sm:text-base">{faq.question}</p>
+                    <div
+                      className={`transition-all duration-400 ${
+                        active ? "rotate-180" : "rotate-0"
+                      }`}
+                    >
+
+                      <FaChevronDown size={25}/>
+                    </div>
+                  </div>
+
+                  <div className="">{faq.answer}</div>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </div>
     </Element>
-  )
+  );
 }
 
 export default FAQ
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -9,20 +9,21 @@ import CustomButton from './CustomButton'
 const Navbar = () => {
   return (
     <nav className='flex justify-between items-center p-5 bg-white z-50 fixed left-0 right-0 top-0 border-b-[1px] border-gray-700/20'>
-        <div className="">
+        <Link to='hero' duration={500} smooth offset={-100} className="cursor-pointer">
             <Image
                 src={"/assets/banner-no-bg.png"}
                 height={30}
                 width={120}
                 alt='logo'
             />
-        </div>
+        </Link>
 
         <div className="flex gap-8 font-semibold">
             {navigation.map((nav) => (
                 <Link
                     key={nav.title}
                     spy
+                    duration={500}
                     smooth
                     activeClass='nav-active'
                     to={nav.url}
